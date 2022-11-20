94a661f07756fcaa9b323f322171fd567aeabd14
coldwater2308 <119cs0034@iiitk.ac.in>
2022-11-20 22:19:45 +0530

    newcommit

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,27 +1,191 @@
-const application = require("../models/projects");
 const axios= require('axios');
-const { sendEmail, sendCustomMail } = require("../Lib/email");
-const client = require("../models/client");
-const register = async (req,res,next)=>{
+const student = require("../models/student");
+const document = require("../models/document");
+const application = require("../models/application");
+const basicDetails = require("../models/basicDetails");
 
+const login = async(req,res,next)=>{
+  let payloadData = req.body;
+  try { 
+   const user =  await student.findOne({id: payloadData.id }) ; 
+  
+    if(user){ 
+      if(await student.matchPassword(password)){
+          const payload={
+              id : student.id
+                        } ;
+          await jwt.sign(
+              payload,
+              process.env.STUDENT_JWT_SECRET,
+              {expiresIn:86400},
+              (error,token)=>{
+                  if(error)
+                      return res.json({message:error});
+                  return res.json({
+                      message :"Success",
+                      token : "Bearer " + token
+                  });
+  
+              }
+          )
+
+  
+       }
+      else { 
+          console.log("Wrong Password");
+      res.status(400).json({error: "Wrong password"});
+      
+    }
+    } 
+    else { 
+        res.status(400).json({error : "Wrong ID"});
+        
+    }
+      
+  } catch (error)
+  {
+     return res.status(200).json(error); 
+  } 
 }
-
 const addEditBasicDetails = async (req,res,next)=>{
+  let payloadData = req.body 
+  try {
+    if(payloadData.id){
+      let id = payloadData.id
+      delete payloadData.id
+      let data = await basicDetails.findByIdAndUpdate(id,payloadData,{new: true})
+      if(data)
+      return res.status(200).json({
+        message:"Success"
+      })
+      else 
+      return res.status(203).json({
+        message:"Something Went Wrong"
+      })
+
+    } 
+    else {
+      let data = new basicDetails(payloadData)
+      await data.save()
+      return res.status(200).json({
+        message: "Success",
+        data : data
+      })
+    }
+  
+    
+  } catch (error) {
+    res.status(400).json({
+      error : error
+    })
+  }
+
 
 }
-const addEdit = async (req,res,next)=>{
+const addEditApplication = async(req,res,next)=>{
+  let payloadData = req.body
+try {
+  if(payloadData.id){
+    let id = payloadData.id
+    delete payloadData.id
+    let data = await application.findByIdAndUpdate(id,payloadData,{new: true})
+    if(data)
+    return res.status(200).json({
+      message:"Success"
+    })
+    else 
+    return res.status(203).json({
+      message:"Something Went Wrong"
+    })
 
+  } 
+  else {
+    let data = new application(payloadData)
+    await data.save()
+    return res.status(200).json({
+      message: "Success",
+      data : data
+    })
+  }
+} catch (error) {
+  res.status(400).json({
+    error : error
+  })
 }
+}
+const getBasicDetails = async(req,res,next)=>{
+  let payloadData = req.body
+  try {
+  let data = await basicDetails.findOne({studentId : payloadData.studentId});
+  if(data)
+  return res.status(200).json({
+    message : "Success",
+    data : data
+  })
+  } catch (error) {
+    res.status(400).json({
+      error : error
+    })
+  }
+}
+const getApplication = async(req,res,next)=>{
+  let payloadData = req.body
+  try {
+  let data = await application.findOne({studentId : payloadData.studentId});
+  if(data)
+  return res.status(200).json({
+    message : "Success",
+    data : data
+  })
+  } catch (error) {
+    res.status(400).json({
+      error : error
+    })
+  }
+}
+const uploadDocuments = async(req,res,next)=>{
+  let payloadData = req.body
+  let file = req.file
+  try {
+    const data = await document.create({
+      appId: payloadData.appId,
+      file:  file.location,
+      name : payloadData.name,
+      type  : payloadData.type
+    });
+    res.status(200).json({
+      message : "Success",
+      data : data
+    })
+  } catch (error) {
+    res.status(400).json({
+      error : error
+    })
+  }
+}
+const getDocuments = async(req,res,next)=>{
+  let payloadData = req.body
+  try {
+  let data = await documents.find({appId : payloadData.appId});
+  if(data)
+  return res.status(200).json({
+    message : "Success",
+    data : data
+  })
+  } catch (error) {
+    res.status(400).json({
+      error : error
+    })
+  }
+} 
 
 
 module.exports = {
-  addEditProject,
-  getProjects,
-  addClient,
-  addRoom,
-  shareProject,
-  verifyUser,
-  sendOTP,
-  verifyOTP,
-  getClients
+  login,
+  addEditBasicDetails,
+  addEditApplication,
+  getBasicDetails,
+  getApplication,
+  uploadDocuments,
+  getDocuments
 };
--- routes/student.js
+++ routes/student.js
@@ -2,15 +2,36 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/student");
 const studentControllers = require("../controllers/studentController");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+aws.config.update({
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+});
 
-// router.post('/addEditTimeline',auth,timelineControllers.addEditTimeline)
-// router.get('/getTimelines' ,auth,timelineControllers.getTimelines )
-// router.post('/addEditItems' ,auth,timelineControllers.addEditItems )
-// router.get('/getItems',auth,timelineControllers.getItems) 
 
+const s3 = new aws.S3();
 
-router.post('/addEditBasicDetails',auth,studentControllers.addEditBasicDetails)
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "3d-idesign",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString()+path.extname(file.originalname));
+    },
+  }),
+});
+
 router.post('/login',studentControllers.login)
+router.post('/addEditBasicDetails',auth,studentControllers.addEditBasicDetails)
+router.post('/addEditApplication',auth, studentControllers.addEditApplication)
+router.get('/getBasicDetails',auth,studentControllers.getBasicDetails)
+router.get('/getApplication',auth, studentControllers.getApplication)
+router.get('/uploadDocuments',auth,upload.single("file", 1),studentControllers.uploadDocuments)
+router.get('/getDocuments',auth,studentControllers.getDocuments)
 router.get('/getDetails',auth,studentControllers.getDetails)
 router.get('/getStatus',auth,studentControllers.getStatus)
+
 module.exports = router;
